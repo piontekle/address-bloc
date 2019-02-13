1c92a5ddc71b4205f631c6895842af71d46ddc34
Lauren Piontek <piontekle@gmail.com>
2019-02-13 13:35:43 -0500

    Checkpoint 3 Complete: add ContactController

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -5,17 +5,6 @@ describe("MenuController", () => {
     this.menu = new MenuController();
   });
 
-  describe("#getContactCount()", () => {
-    it("should return 0 when no contacts are in the book", () => {
-      expect(this.menu.getContactCount()).toBe(0);
-    });
-
-    it("should return 1 when there is exactly one contact in the book", () => {
-      this.menu.contacts.push("Bob");
-      expect(this.menu.getContactCount()).toBe(1);
-    });
-  });
-
   describe("#remindMe", () => {
     it("should return reminder of life-long learning", () => {
       expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
